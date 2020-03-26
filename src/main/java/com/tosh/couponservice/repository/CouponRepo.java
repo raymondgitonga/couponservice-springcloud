5a865639b5c9430d8cb48bc29f62a11343347ee6
raymond gitonga <raytosh95@gmail.com>
2020-03-26 04:14:22 +0300

    Add Coupon entity and repository

---
+++ src/main/java/com/tosh/couponservice/repository/CouponRepo.java
@@ -0,0 +1,7 @@
+package com.tosh.couponservice.repository;
+
+import com.tosh.couponservice.model.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponRepo extends JpaRepository<Coupon, Long> {
+}
