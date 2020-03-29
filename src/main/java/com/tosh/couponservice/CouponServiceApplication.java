c99e0287c576949dbddb58f6ce18a8ee2571c36e
raymond gitonga <raytosh95@gmail.com>
2020-03-30 02:16:39 +0300

    fix

--- src/main/java/com/tosh/couponservice/CouponServiceApplication.java
+++ src/main/java/com/tosh/couponservice/CouponServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class CouponserviceApplication {
+public class CouponServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CouponserviceApplication.class, args);
+		SpringApplication.run(CouponServiceApplication.class, args);
 	}
 
 }
--- src/test/java/com/tosh/couponservice/CouponServiceApplicationTests.java
+++ src/test/java/com/tosh/couponservice/CouponServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CouponserviceApplicationTests {
+class CouponServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
